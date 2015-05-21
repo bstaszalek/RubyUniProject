95b4d46f880b9e3d159325733a4bcb66f2db9258
BStaszalek <bartosz.staszalek@gmail.com>
2015-05-21 08:39:09 +0000

    Change in project controller (index, edit, create, destroy)

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,12 +1,31 @@
 class ProjectsController < ApplicationController
+  
+  def index
+    @projects = Project.all
+  end
+  
   def new
+    @project = Project.new
   end
   
   def create
     @project = Project.new(project_params)
  
-    @project.save
-    redirect_to @project
+    if @project.save
+      redirect_to @project
+    else
+      render 'new'
+  end
+  
+  def edit
+    @project = Project.find(params[:id])
+  end
+  
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+
+    redirect_to projects_path
   end
   
   private
