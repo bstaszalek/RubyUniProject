7e5ce8337854a59f1de145f16a86048d340037c2
BStaszalek <bartosz.staszalek@gmail.com>
2015-05-30 00:13:52 +0000

    Add some views for project, typo in ticket model

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,6 +1,8 @@
 class Ticket < ActiveRecord::Base
   belongs_to :project
-  vaildates :title, presence: true, length: {minimum: 5}
+
+  validates :title, presence: true, length: {minimum: 5}
   validates :description, presence: true
   validates :priority, presence: true
+  
 end
