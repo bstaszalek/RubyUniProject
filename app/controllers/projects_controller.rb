b0545763162c17daab13e43e1056e5d54b85955f
BStaszalek <bartosz.staszalek@gmail.com>
2015-06-01 20:07:06 +0000

    Change in appearance

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,4 @@
 class ProjectsController < ApplicationController
-  
   def index
     @projects = current_user.projects.all
   end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,9 +4,10 @@ class Ability
   def initialize(user)
     user ||= User.new # guest user (not logged in)
     if user.admin?
-      can :manage, Project
-      can :manage, Ticket
+
     else
+      can :create, Project
+      can :manage, Project
       can :create, Ticket
       can :manage, Ticket do |ticket|
         comment.try(:ticketer) == user.email
